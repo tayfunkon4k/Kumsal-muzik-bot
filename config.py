585ff19561af2bb766570087871d41fd1dc1646a
TAYFUN <87475699+tayfunkon4k@users.noreply.github.com>
2024-12-27 23:19:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -35,17 +35,17 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/mamakli06/mamaklimusic1",
+    "https://github.com/tayfunkon4k/kumsal-muzık-bot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.mamaklibirininruhu")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/mamaklimekani")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SefillerYeniden")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SefillerKurucu")
 
-# Set this to True if you want the assistant to automatically leave chats after an interval
+# Asistanın belirli bir süre sonunda sohbetlerden otomatik olarak ayrılmasını istiyorsanız bunu Doğru olarak ayarlayın.
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
 
